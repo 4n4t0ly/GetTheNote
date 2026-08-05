45eef2cf13e14029f3a571aab62a434889293a86
Анатолий Боровик <anatoly.borovik95@gmail.com>
2026-08-05 15:09:04 +0300

    No diolog anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
-#include "dialog.h"
+#include "../include/mainwindow.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "GetTheNote_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    Dialog w;
+    MainWindow w;
     w.show();
     return QApplication::exec();
 }
---
+++ src/mainwindow.cpp
@@ -0,0 +1,9 @@
+#include "../include/mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+}
+
+MainWindow::~MainWindow() = default;
+
