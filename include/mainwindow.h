82b3d5cc6e2da33021ec5ba9fda544e05aa2dae5
Анатолий Боровик <anatoly.borovik95@gmail.com>
2026-08-13 12:12:01 +0300

    Was added pre-historical UI

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QFileDialog>
+#include <QHBoxLayout>
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +18,16 @@ private slots:
     void handleButton();
 private:
     QString fileName;
+
+    QVBoxLayout *generalLayout;
     QPushButton *openButton;
     QLabel *nameLabel;
+
+    QWidget *centralWidget;
+    QPushButton *clearButton;
+    QPushButton *repeatButton;
+    QPushButton *goBackButton;
+    QPushButton *pauseButton;
+    QHBoxLayout *controlPanelLayout;
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "../include/mainwindow.h"
-constexpr int minWindowWidth = 600;
-constexpr int minWindowHeigth = 350;
+constexpr unsigned short int minWindowWidth = 600;
+constexpr unsigned short int minWindowHeigth = 350;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent){
     setMinimumSize(minWindowWidth, minWindowHeigth);
-    openButton = new QPushButton("Open", this);
-    openButton->setGeometry(QRect(QPoint(0, 0), QSize(200, 100)));
-    nameLabel = new QLabel(fileName, this);
-    nameLabel->setGeometry(QRect(QPoint(200,200),QSize(100,100)));
+    centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    generalLayout = new QVBoxLayout(centralWidget);
+    controlPanelLayout = new QHBoxLayout();
+
+    openButton = new QPushButton("Open");
+    nameLabel = new QLabel(fileName);
+
+    generalLayout->addWidget(openButton);
+    generalLayout->addWidget(nameLabel);
+    generalLayout->addLayout(controlPanelLayout);
     connect(openButton, &QPushButton::released, this, &MainWindow::handleButton);
+
+    clearButton = new QPushButton("Clear");
+    repeatButton = new QPushButton("Repeat");
+    goBackButton = new QPushButton("Go Back");
+    pauseButton = new QPushButton("Pause");
+
+    controlPanelLayout->addWidget(clearButton);
+    controlPanelLayout->addWidget(repeatButton);
+    controlPanelLayout->addWidget(goBackButton);
+    controlPanelLayout->addWidget(pauseButton);
 }
 void MainWindow::handleButton(){
     fileName = QFileDialog::getOpenFileName(this,
